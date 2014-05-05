787928fa01d72ae9c7725e598eddfad18da5a83f
KobeSG <657885964@qq.com>
2014-05-05 13:12:46 +0800

    增加了InputStream

--- TouchLogger/src/zju/ccnt/xptools/syn/SynRunnable.java
+++ TouchLogger/src/zju/ccnt/xptools/syn/SynRunnable.java
@@ -1,23 +1,32 @@
 package zju.ccnt.xptools.syn;
 
+import java.io.InputStream;
+
 public abstract class SynRunnable implements Runnable {
 	protected Class model;
-	
-	
+
+	protected InputStream inputStream;
+
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+
+	public void setInputStream(InputStream inputStream) {
+		this.inputStream = inputStream;
+	}
+
 	/*
-	 * Get the Class presentation of the model whose
-	 * instance is to be stored.
+	 * Get the Class presentation of the model whose instance is to be stored.
 	 */
 	public Class getModelClass() {
 		return model;
 	}
 
 	/*
-	 * Set the Class presentation of the model whose
-	 * instance is to be stored.
+	 * Set the Class presentation of the model whose instance is to be stored.
 	 */
 	public void setModelClass(Class model) {
-		this.model=model;
+		this.model = model;
 	}
 
 	public SynRunnable(Class model) {
@@ -25,7 +34,7 @@ public abstract class SynRunnable implements Runnable {
 	}
 
 	/*
-	 *	Details of reading file and upload to server.
+	 * Details of reading file and upload to server.
 	 */
 	@Override
 	abstract public void run();
